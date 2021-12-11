2678a1e0c96a7cfe08e97e402f060fb22ab53dec
Diego Balbis <dbalbis8@gmail.com>
2021-12-11 00:19:56 -0300

    todo pronto para entregar

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -1,3 +1,28 @@
+/* ARRAYS */
+
+const menu = ['[1] Milanesas ', '[2] Asado ', '[3] Ensalada '];
+
+/* CONSTRUCTOR */
+
+class Plato {
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+  listarPlatos() {
+    alert(
+      'El plato que pediste es: ' +
+        this.nombre +
+        ' y el precio es: ' +
+        this.precio
+    );
+  }
+}
+
+const Plato1 = new Plato('Milanesas', '$500');
+const Plato2 = new Plato('Asado', '$650');
+const Plato3 = new Plato('Ensalada', '$700');
+
 /* FUNCION -SOLO PARA MODO TAKE AWAY */
 
 function extra(num2) {
@@ -13,8 +38,8 @@ alert('Te traemos los mas finos sabores Arabes a la puerta de tu casa.');
 
 let modo = +prompt('[1] Desea Take-Away o [2] Delivery?');
 
-while (modo == null || /\D/.test(modo) || modo == '') {
-  modo = +prompt('Debe Ingresar: [1] Take-Away o [2] Delivery');
+while (modo > 2 || modo < 0 || modo == null || /\D/.test(modo) || modo == '') {
+  modo = +prompt('[1] Desea Take-Away o [2] Delivery?');
 }
 
 /* MODO TAKE AWAY */
@@ -30,9 +55,15 @@ if (modo === 1) {
   let bandeja = +prompt(
     'Nuestro menu es: Bandeja[1]: $400 Bandeja[2]: $560 Bandeja[3]: $1100. Ingrese el número de bandeja:'
   );
-  while (bandeja == null || /\D/.test(bandeja) || bandeja == '') {
+  while (
+    bandeja > 3 ||
+    bandeja < 1 ||
+    bandeja == null ||
+    /\D/.test(bandeja) ||
+    bandeja == ''
+  ) {
     bandeja = +prompt(
-      'Debe Ingresar:  Bandeja[1]: $400 Bandeja[2]: $560 Bandeja[3]: $1100.'
+      'Nuestro menu es: Bandeja[1]: $400 Bandeja[2]: $560 Bandeja[3]: $1100. Ingrese el número de bandeja:'
     );
   }
 
@@ -72,31 +103,36 @@ if (modo === 2) {
   while (direccion == null || direccion == '') {
     direccion = prompt('Ingrese su dirección:');
   }
-  let plato = +prompt(
-    'Nuestros Platos son: Plato[1]: $350 Plato[2]: $500 Plato[3]: $1050. - Ingrese el número del plato:'
-  );
+  let plato = +prompt('Ingrese el numero: ' + menu);
 
-  while (plato == null || /\D/.test(plato) || plato == '') {
-    plato = +prompt(
-      'Debe Ingresar: Plato[1]: $350 Plato[2]: $500 Plato[3]: $1050.'
-    );
+  while (
+    plato > 3 ||
+    plato < 1 ||
+    plato == null ||
+    /\D/.test(plato) ||
+    plato == ''
+  ) {
+    plato = +prompt('Ingrese el numero: ' + menu);
   }
   switch (plato) {
     case 1:
+      Plato1.listarPlatos();
       alert(
-        `Estamos preprando su Plato. Total: $305 se enviara a: ${direccion}. Disfruta la comida!`
+        `Estamos preprando su Plato. Se enviara a: ${direccion}. Disfruta la comida!`
       );
       alert('Gracias, Chauu!');
       break;
     case 2:
+      Plato2.listarPlatos();
       alert(
-        `Estamos preprando su Plato. Total: $500 se enviara a: ${direccion}. Disfruta la comida!`
+        `Estamos preprando su Plato. Se enviara a: ${direccion}. Disfruta la comida!`
       );
       alert('Gracias, Chauu!');
       break;
     case 3:
+      Plato3.listarPlatos();
       alert(
-        `Estamos preprando su Plato. Total: $1050 se enviara a: ${direccion}. Disfruta la comida!`
+        `Estamos preprando su Plato. Se enviara a: ${direccion}. Disfruta la comida!`
       );
       alert('Gracias, Chauu!');
       break;
