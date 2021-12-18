57363f38dec0545e07b5ee3ab7bbb39039efba3e
Diego Balbis <dbalbis8@gmail.com>
2021-12-17 23:29:21 -0300

    listo el desafio de HTML

--- js/index.js
+++ js/index.js
@@ -1,38 +1,55 @@
 /* ARRAYS */
 
-const menu = ['[1] Milanesas ', '[2] Asado ', '[3] Ensalada '];
+/* const menu = ['[1] Milanesas ', '[2] Asado ', '[3] Ensalada ']; */
 
 /* CONSTRUCTOR */
 
-class Plato {
-  constructor(nombre, precio) {
-    this.nombre = nombre;
-    this.precio = precio;
-  }
-  listarPlatos() {
-    alert(
-      'El plato que pediste es: ' +
-        this.nombre +
-        ' y el precio es: ' +
-        this.precio
-    );
-  }
+function Plato(id, nombre, stock, precio, img) {
+  this.id = id;
+  this.nombre = nombre;
+  this.stock = stock;
+  this.precio = precio;
+  this.img = img;
 }
 
-const Plato1 = new Plato('Milanesas', '$500');
-const Plato2 = new Plato('Asado', '$650');
-const Plato3 = new Plato('Ensalada', '$700');
+let Plato1 = new Plato('1', 'Plato 1:', '50', '$500', './img/plato1.jpg');
+let Plato2 = new Plato('2', 'Plato 2:', '60', '$650', './img/plato2.jpg');
+let Plato3 = new Plato('3', 'Plato 3:', '70', '$700', './img/plato3.jpg');
+
+let misPlatos = [];
+
+misPlatos.push(Plato1, Plato2, Plato3);
+
+/* DOM */
+
+let contenedorPlatos = document.getElementById('contenedor-platos');
+
+for (let plato of misPlatos) {
+  let div = document.createElement('div');
+  div.setAttribute('class', 'plato');
+
+  div.innerHTML = `
+<h3> ${plato.nombre} ${plato.precio} </h3>
+<img src="${plato.img}" alt="" width="200">
+</br>
+<button type="button" class="button" id="producto${plato.id}">Comprar</button>
+`;
+
+  contenedorPlatos.appendChild(div);
+}
+
+/* ESTO CREO QUE YA NO SIRVE MAS */
 
 /* FUNCION -SOLO PARA MODO TAKE AWAY */
 
-function extra(num2) {
+/* function extra(num2) {
   var num2 = 80;
   suma = precio + num2;
-}
+} */
 
 /* BIENVENIDA */
 
-alert('Bienvenido a Abu Dhabi. Déjate sorprender con nuestros platos.');
+/* alert('Bienvenido a Abu Dhabi. Déjate sorprender con nuestros platos.');
 
 alert('Te traemos los mas finos sabores Arabes a la puerta de tu casa.');
 
@@ -41,10 +58,10 @@ let modo = +prompt('[1] Desea Take-Away o [2] Delivery?');
 while (modo > 2 || modo < 0 || modo == null || /\D/.test(modo) || modo == '') {
   modo = +prompt('[1] Desea Take-Away o [2] Delivery?');
 }
-
+ */
 /* MODO TAKE AWAY */
 
-if (modo === 1) {
+/* if (modo === 1) {
   alert(
     'Los pedidos para retirar en el local tienen un costo de $80 extra por las bandejas de plastico.'
   );
@@ -95,10 +112,10 @@ if (modo === 1) {
       break;
   }
 }
-
+ */
 /* MODO DELIVERY */
 
-if (modo === 2) {
+/* if (modo === 2) {
   direccion = prompt('Ingrese su dirección:');
   while (direccion == null || direccion == '') {
     direccion = prompt('Ingrese su dirección:');
@@ -138,3 +155,4 @@ if (modo === 2) {
       break;
   }
 }
+ */
